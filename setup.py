8ec9da15dfcc7fc1a2ca387639cf288ecf8e8b80
archana53 <archanakutumbaka7@gmail.com>
2024-01-09 10:56:07 -0500

    [Setup] Installing ControlNet as a package enabled!

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="controlnet",
+    version="0.1",
+    description="ControlNet Implementation",
+    url="https://github.com/lllyasviel/ControlNet/",
+    packages=setuptools.find_packages(),
+)
